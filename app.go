e79e3c0cc139dfaf202a30cd177d84d07e262e64
andikaarbillah <chrandika3@gmail.com>
2023-11-18 18:45:26 +0700

    completed challenge live code

--- app.go
+++ app.go
@@ -1,5 +1,8 @@
 package main
 
-func main() {
+import "enigmacamp.com/be-enigma-laundry/delivery"
 
+func main() {
+	delivery.NewServer().Run()
 }
+
--- delivery/server.go
+++ delivery/server.go
@@ -1,1 +1,51 @@
 package delivery
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+
+	"enigmacamp.com/be-enigma-laundry/config"
+	"enigmacamp.com/be-enigma-laundry/delivery/controller"
+	"enigmacamp.com/be-enigma-laundry/manager"
+)
+
+type Server struct {
+	uc     manager.UseCaseManager
+	engine *gin.Engine
+	host   string
+}
+
+func (s *Server) setupControllers() {
+	rg := s.engine.Group("/api/v1")
+	controller.NewBillController(s.uc.BillUseCase(), rg).Route()
+}
+
+func (s *Server) Run() {
+	s.setupControllers()
+	if err := s.engine.Run(s.host); err != nil {
+		log.Fatal("server can't run")
+	}
+}
+
+func NewServer() *Server {
+	cfg, err := config.NewConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	infra, err := manager.NewInfraManager(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	repo := manager.NewRepoManager(infra)
+	uc := manager.NewUseCaseManager(repo)
+	engine := gin.Default()
+	host := fmt.Sprintf(":%s", cfg.ApiPort)
+	return &Server{
+		uc:     uc,
+		engine: engine,
+		host:   host,
+	}
+}
